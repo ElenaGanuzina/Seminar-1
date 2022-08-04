6dec811ba5005e53c2c9a3b8c7957cc87591883c
ElenaG <freshelf@yandex.ru>
2022-08-05 00:22:06 +0500

    add Task 004

---
+++ OneDrive/Рабочий стол/Seminar/Task004/Program.cs
@@ -0,0 +1,12 @@
+﻿// Вход - целое N, выход - {-N...N}
+
+Console.WriteLine("Введите любое целое положительное число");
+
+int number = Convert.ToInt32(Console.ReadLine());
+int count = number;
+Console.Write(number.ToString() + " ");
+while(count > -number)
+{
+    number = number - 1;
+    Console.Write(number.ToString() + " ");
+}
