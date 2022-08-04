20e6479d543fdc6a53927a8ee28dc3851ab4d0da
ElenaG <freshelf@yandex.ru>
2022-08-05 00:20:01 +0500

    add Task 002

---
+++ OneDrive/Рабочий стол/Seminar/Task002/Program.cs
@@ -0,0 +1,9 @@
+﻿// Проверить, является ли первое число квадратом второго
+
+Console.WriteLine ("Введите число 1");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine ("Введите число 2");
+int b = Convert.ToInt32(Console.ReadLine());
+
+if (a == b * b) Console.WriteLine("верно");
+else Console.WriteLine("неверно");
