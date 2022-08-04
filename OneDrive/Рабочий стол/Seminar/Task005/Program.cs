51fa8d2d278d0b02cb36c6a4f8b9f4fcac19c4b7
ElenaG <freshelf@yandex.ru>
2022-08-05 00:22:43 +0500

    add Task 005

---
+++ OneDrive/Рабочий стол/Seminar/Task005/Program.cs
@@ -0,0 +1,13 @@
+﻿// 3-х значное число, показать последнюю цифру
+
+Console.WriteLine("Введите трехзначное число");
+int number = Convert.ToInt32(Console.ReadLine());
+if (number >=100 && number < 1000)
+{
+    int digit = number % 10;
+    System.Console.WriteLine($"Последняя цифра числа {number} равна {digit}");
+}
+else
+{
+    System.Console.WriteLine("Введенное число не трехзначное");
+}
